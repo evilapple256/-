5365a5bab48daa1c5e06098785e43c2d828de41b
evilapple256 <kogata0703@gmail.com>
2022-10-02 19:09:41 +0900

    Delete sec_to_time.cpp

--- C++/sec_to_time.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-	int a;
-	printf("時間を入力してください\n");
-	scanf_s("%d", &a);
-	printf("%d秒は%d時間%d分%d秒です", a, a / 3600, (a - (a / 3600) * 3600) / 60, a - (a / 60) * 60);
-}
